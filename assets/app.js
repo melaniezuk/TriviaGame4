e8f0a081665128dda94adafcbf7ba0a85113f637
MelZee <zenani3@gmail.com>
2018-05-21 13:34:22 -0400

    revision

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+//once the "start" button from the first page is clicked, it goes to the second screen
+
+//there is a counter or timer that is counting down from 120 seconds after "start" is clicked.
+
+//on the second screen, the user picks one answer for each of the 8 questions
+
+//the counter counts down from 120 to zero while the user plays the game.
+
+//Answers are shown once the time is up or the "done" button is pushed.
+
+
+
+//to calculate answers
+
+//All correct answers are identified
+
+//when the correct answers are clicked, one point is added to the correct category.
+
+//when the incorrect answer is clicked, one point is added to the incorrect category. 
+
+//when a question is left unanswered, one point is added to the unanswered category. 
