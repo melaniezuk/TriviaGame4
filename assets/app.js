1d9bfb493e7ae163bfca89ec02c81eee1f28104b
MelZee <zenani3@gmail.com>
2018-05-24 11:53:30 -0400

    for Stephanie

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,32 @@
 /*timer function*/
-setTimeout(sixtySeconds, 1000 * 60);
+/*setTimeout(sixtySeconds, 1000 * 60);
 
     function sixtySeconds(){
         $("#time-left").append("<h2>Time's Up!</h2>");
         console.log("time is up");
-    }
-
+    }*/
+    
 $(document).ready(function(){
     console.log("document is loaded")
+
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 60);
+        }
+    
+        function decrement() {
+            number--;
+            $("#show-number").html("<h2>" + number + "</h2>");
+            if (number === 0) {
+                stop();
+                alert("Time's Up!");
+            
+            }
+        }
+    
+        function stop() {
+            clearInterval(intervalId);
+        }
 
         
     $("#submit").click(function(){
@@ -93,39 +112,5 @@ $(document).ready(function(){
     
 }) 
 
-/* Here is my other timing code-I know the first 7 lines should be in HTML, but the rest is js
 
-<div id="show-number"></div>
 
-<br>
-
-<button id="start">Start!</button>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript">
-
-var number = 60;
-var intervalId;
-
-    $("#start").on("click", run);
-
-    function run() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 60);
-        }
-
-        function decrement() {
-            number--;
-            $("#show-number").html("<h2>" + number + "</h2>");
-            if (number === 0) {
-                stop();
-                alert("Time's Up!");
-                
-            }
-        }
-
-        function stop() {
-            clearInterval(intervalId);
-        }
-
-        run();
