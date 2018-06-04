ae63481fc6ec41baaab6a94ac59fa123d5d76414
Sam Sawan <ssawan@onshift.com>
2018-06-04 18:37:14 -0400

    bit of the ol refactor

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-   
+
 $(document).ready(function(){
     console.log("document is loaded")
 
@@ -17,97 +17,74 @@ $(document).ready(function(){
         $("#show-number").html("<h2>" + number + "</h2>");
         if (number === 0) {
             stop();
-            alert("Time's Up!");                   
+            alert("Time's Up!");
             }
         }
 
     function getUnanswered (correctTally, incorrectTally) {
             alert (totalQuestion - sum);
-        }  
-        
+        }
+
     function stop() {
         clearInterval(intervalId);
     }
-           
+
     $("#submit").click(function(){
-       
+
         var correctTally = 0;
         var incorrectTally = 0;
         var getUnanswered = 0;
         var totalQuestion = 8;
         var sum = correctTally + incorrectTally
-        
+
+        function determiner(scope){
+            if(scope.data("answer")){
+                correctTally++;
+            }else{
+                incorrectTally++;
+            }
+        };
+
         $("input[name='q1']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++
-            }else{
-                incorrectTally++
-            }
+            determiner($(this));
         });
-          
+
         $("input[name='q2']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;
-            }
+            determiner($(this));
         });
-        
+
         $("input[name='q3']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++; 
-            }
+            determiner($(this));
         });
-   
+
         $("input[name='q4']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;
-            }
+            determiner($(this));
         });
 
         $("input[name='q5']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;
-            }
+            determiner($(this));
         });
 
         $("input[name='q6']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;
-            }
+            determiner($(this));
         });
 
         $("input[name='q7']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;
-            }
+            determiner($(this));
+
         });
 
         $("input[name='q8']:checked").each(function(){
-            if($(this).data("answer")){
-                correctTally++;
-            }else{
-                incorrectTally++;   
-            }
+            determiner($(this));
         });
-        
+
         alert("Number of Correct Answers = " + correctTally + " " + "Number of Incorrect Answers = " + incorrectTally + " " + "Number of Unanswered questions = " + getUnanswered);
-              
-    
-       
+
+
+
     });
-    
-}) 
+
+})
 
 
 
